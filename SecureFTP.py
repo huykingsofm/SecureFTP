ba1e34bc199fc5a4743f911a14c83607e0b52415
Le Huy <huykingsofm@gmail.com>
2020-11-21 03:28:00 +0700

    Combine SFTPServer and SFTPClient to SFTP, no longer server and client, now they are sender and receiver

--- SecureFTP.py
+++ SecureFTP.py
@@ -6,6 +6,12 @@ from .LocalVNetwork import StandardPrint
 from .LocalVNetwork.DefinedError import InvalidArgument
 from .LocalVNetwork.SecureTCP import STCPSocketClosed
 
+def __check_ip__(ip):
+    try:
+        socket.inet_aton(ip)
+        return True
+    except:
+        return False
 
 def __hash_a_file__(filename, buffer_size = 10 ** 6):
     sha1 = hashlib.sha1(b"")
@@ -14,28 +20,90 @@ def __hash_a_file__(filename, buffer_size = 10 ** 6):
         sha1.update(data)
     return sha1.digest()
 
-class SFTPServer(object):
-    def __init__(self, address, newfilename, cipher, save_file_after: int = 65536, buffer_size: int = 1024, verbosities:tuple = ("error", )):
-        self.address = address
-        self.newfilename = newfilename
-        self.save_file_after = save_file_after
-        self.buffer_size = buffer_size
-        self.cipher = cipher
-        self.__print__ = StandardPrint(f"From SFTP {address}", verbosities)
+class SFTP(object):
+    def __init__(self, address, address_onwer, verbosities = ("error", "warning")):
+        """
+        address ~ (ip, port)
+        address_onwer ~ "self" or "partner"
+        """
+        assert isinstance(address, tuple), "Address must be a tuple"
+        assert len(address) == 2, "Address must be (ip, port) format"
+        assert __check_ip__(address[0]), "Invalid IP ({})".format(address[0])
+        assert isinstance(address[1], int) and address[1] >= 0 and address[1] <= 65535, "Invalid port ({})".format(address[1])
+        assert address_onwer in ["self", "partner"], "Address owner must be only \"self\" or \"owner\""
 
-    def _wait(self):
-        self.socket = STCPSocket(cipher= self.cipher, buffer_size= self.buffer_size, verbosities= ("error", "warning"))
-        self.socket.bind(self.address)
-        self.socket.listen()
-        return self.socket.accept()
+        self.__address__ = address
+        self.__address_owner__ = address_onwer
+        self.__print__ = StandardPrint("From SFTP", verbosities = verbosities)
     
-    def start(self):
-        client, address = self._wait()
-        self.__print__(f"Connected to {address}", "notification")
+    def __connect__(self):
+        s = STCPSocket(
+            cipher= self.__cipher__, 
+            buffer_size= self.__buffer_size__, 
+            verbosities= ("error", "warning")
+        )
+        if self.__address_owner__ == "self":
+            s.bind(self.__address__)
+            s.listen()
+            self.__socket__, partner_address = s.accept()
+            self.__print__("{} connected".format(partner_address), "notification")
+            s.close()
+        else:
+            s.connect(self.__address__)
+            self.__socket__ = s
+            self.__print__("Connect to {} successfully".format(self.__address__), "notification")
 
+    def __send__(self):
+        fstream = open(self.__file_name__, "rb")
+        total_size = 0
+        file_size = os.path.getsize(self.__file_name__)
+        file_size = file_size.to_bytes(8, "big")
+        data = self.__socket__.recv()
+        if data == b"$request file_size":
+            self.__socket__.send(file_size)
+        else:
+            return False
+
+        sha1 = __hash_a_file__(self.__file_name__)
+        data = self.__socket__.recv()
+        if data == b"$request file_hash":
+            self.__socket__.send(sha1)
+        else:
+            return False
+
+        complete = False
+        while True:
+            try:
+                data = fstream.read(self.__buffer_size__)
+                if not data:
+                    complete = True
+                    break
+                self.__socket__.send(data)
+                total_size += len(data)
+                self.__print__(f"sent total {total_size} bytes", "notification")
+                self.__socket__.recv()
+            except Exception as e:
+                self.__print__(repr(e), "error")
+                break
+        
+        self.__socket__.send(b"$sending_complete")
         try:
-            client.send(b"$request file_size")
-            data = client.recv()
+            if not complete:
+                return False
+
+            data = self.__socket__.recv()
+            if data != b"$result success":
+                return False
+
+            return True
+        finally:
+            fstream.close()
+            self.__socket__.close()
+
+    def __receive__(self):
+        try:
+            self.__socket__.send(b"$request file_size")
+            data = self.__socket__.recv()
         except STCPSocketClosed:
             return False
         except Exception as e:
@@ -45,8 +113,8 @@ class SFTPServer(object):
         expected_file_size = int.from_bytes(data, "big")
 
         try:
-            client.send(b"$request file_hash")
-            expected_sha1 = client.recv()
+            self.__socket__.send(b"$request file_hash")
+            expected_sha1 = self.__socket__.recv()
         except STCPSocketClosed:
             return False
         except Exception as e:
@@ -55,11 +123,11 @@ class SFTPServer(object):
 
         current_size = 0
         total_size = 0
-        fstream = open(self.newfilename, "wb")
+        fstream = open(self.__storage_path__, "wb")
 
         while True:
             try:
-                data = client.recv()
+                data = self.__socket__.recv()
             except STCPSocketClosed:
                 break
             except Exception as e:
@@ -69,17 +137,20 @@ class SFTPServer(object):
             if not data:
                 continue
 
+            if data == b"$sending_complete":
+                break
+
             fstream.write(data)
             current_size += len(data)
             total_size += len(data)
-            if current_size >= self.save_file_after:
+            if current_size >= self.__save_file_after__:
                 fstream.close()
-                fstream = open(self.newfilename, "ab")
+                fstream = open(self.__storage_path__, "ab")
                 current_size = 0
             self.__print__(f"received total {total_size} bytes", "notification")
 
             try:
-                client.send(f"$done {total_size}".encode())
+                self.__socket__.send(f"$done {total_size}".encode())
             except STCPSocketClosed:
                 break
             except Exception as e:
@@ -87,75 +158,53 @@ class SFTPServer(object):
                 break
 
         fstream.close()
-        client.close()
-        self.socket.close()
 
+        result = True
+        message = ""
         if total_size < expected_file_size:
             self.__print__("Not enough file size", "notification")
-            return False
+            message = "Not enough file size"
+            result = False
 
         if total_size > expected_file_size:
             self.__print__("File size is larger than expected", "notification")
-            return False
+            message = "File size is larger than expected"
+            result = False
 
-        sha1 = __hash_a_file__(self.newfilename)
+        sha1 = __hash_a_file__(self.__storage_path__)
         if sha1 != expected_sha1:
             self.__print__("Non-integrity file", "notification")
-            return False
+            message = "File integrity is compromised"
+            result = False
 
-        self.__print__(f"Received file successfully, save at {self.newfilename}", "notification")
-        return True
+        self.__print__(f"Received file successfully, save at {self.__storage_path__}", "notification")
 
-class SFTPClient(object):
-    def __init__(self, server_address, filename, cipher, buffer_size = 65535, verbosities: tuple = ("error", )):
-        self.server_address = server_address
-        self.filename = filename
-        self.buffer_size = buffer_size
-        self.cipher = cipher
-        self.__print__ = StandardPrint("From SFTP Client", verbosities)
+        if result == True:
+            self.__socket__.send(b"$result success")
+        else:
+            self.__socket__.send(b"$result failure " + message.encode())
+        self.__socket__.close()
+        return result
 
-    def _connect(self):
-        self.socket = STCPSocket(cipher= self.cipher, verbosities = ("error", "warning"))
-        self.socket.connect(self.server_address)
-    
+    def as_sender(self, file_name, cipher, buffer_size = 65535):
+        self.__file_name__ = file_name
+        self.__cipher__ = cipher
+        self.__buffer_size__ = buffer_size
+        self.__role__ = "sender"
+
+    def as_receiver(self, storage_path, cipher, save_file_after: int = 65536, buffer_size: int = 1024):
+        self.__storage_path__ = storage_path
+        self.__cipher__ = cipher
+        self.__save_file_after__ = save_file_after
+        self.__buffer_size__ = buffer_size
+        self.__role__ = "receiver"
+
     def start(self):
-        self._connect()
-        fstream = open(self.filename, "rb")
-        total_size = 0
-        file_size = os.path.getsize(self.filename)
-        file_size = file_size.to_bytes(8, "big")
-        data = self.socket.recv()
-        if data == b"$request file_size":
-            self.socket.send(file_size)
+        self.__connect__()
+        if self.__role__ == "sender":
+            return self.__send__()
         else:
-            return False
-
-        sha1 = __hash_a_file__(self.filename)
-        data = self.socket.recv()
-        if data == b"$request file_hash":
-            self.socket.send(sha1)
-        else:
-            return False
-
-        flag = False
-        while True:
-            try:
-                data = fstream.read(self.buffer_size)
-                if not data:
-                    flag = True
-                    break
-                self.socket.send(data)
-                total_size += len(data)
-                self.__print__(f"sent total {total_size} bytes", "notification")
-                self.socket.recv()
-            except Exception as e:
-                self.__print__(repr(e), "error")
-                break
-            
-        self.socket.close()
-        fstream.close()
-        
-        return flag
+            return self.__receive__()
 
 if __name__ == "__main__":
     print(__hash_a_file__("a"))
